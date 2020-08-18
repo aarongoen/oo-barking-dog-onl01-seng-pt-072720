64a96f98235331a6d00d007ebd71bd08999a5ace
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-18 12:19:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 # Your code goes here!
 class Dog
+<<<<<<< HEAD
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
@@ -9,6 +10,17 @@ class Dog
   end
   
   def bark
+=======
+  def .name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def .name
+    this_dogs_name
+  end
+  
+  def .bark
+>>>>>>> 762de6f4e9ee58814d9ceeb910e78a0612e68a1f
     puts "woof!"
   end 
 end 
